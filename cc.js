0a257f689c3ff887fd5ec6c08e5a40d047a19600
Morodekaii <morodekaiibyimanikora@gmail.com>
2026-05-14 16:28:47 -0800

    onather edited commit

---
+++ cc.js
@@ -0,0 +1,11 @@
+
+    function showImage(){
+        document.getElementById("myImage").style.display="block";
+        document.getElementById("seemorebtn").style.display="none";
+        document.getElementById("seelessbtn").style.display="hidden";
+    }
+    function hideImage(){
+        document.getElementById("myImage").style.display="none";
+        document.getElementById("seemorebtn").style.display="inline-block";
+        document.getElementById("seelessbtn").style.display="none";
+    }
